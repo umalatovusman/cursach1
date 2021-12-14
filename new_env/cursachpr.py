a23e857601c301e22b227cb52fe3444c9551b2fb
Usman Umalatov <umalatov.usman@bk.ru>
2021-12-14 21:24:03 +0300

    add files and finish cursach1

--- new_env/cursachpr.py
+++ new_env/cursachpr.py
@@ -1,31 +1,74 @@
 import requests
 from Wappalyzer import Wappalyzer, WebPage
+import ssl, socket
+
 # домен для поиска поддоменов
-domain = "pgu.ru"
+domain = "https://pgu.ru/"
 # читать все поддомены
-file = open("subdomains-1000.txt")
-# прочитать весь контент
-content = file.read()
-# разделить на новые строки
-subdomains = content.splitlines()
-# список обнаруженных поддоменов
-discovered_subdomains = []
-
-for subdomain in subdomains:
-    # создать URL
-
-    url = f"http://{subdomain}.{domain}"
+def get_cms(urls):
+    result = {}
+    cms_found = None
+    wappalyzer = Wappalyzer.latest()
     try:
-        # если возникает ОШИБКА, значит, субдомен не существует
-        requests.head(url, timeout=5)
-    except requests.ConnectionError:
-        # если поддомена не существует, просто передать, ничего не выводить
+        webpage = WebPage.new_from_url(urls)
+        result = wappalyzer.analyze_with_categories(webpage)
+    except Exception as E:
         pass
-    else:
-        print("[+] Обнаружен поддомен:", url)
-        discovered_subdomains.append(url)
+
+    for key, key1 in result.items():
+        for keys, values in key1.items():
+            if values == ["cms"]:
+                print("{0} : {1}".format(urls, key))
+                cms_found = 1
+                return urls + " : " + key
+            else:
+                cms_found = None
+                continue
+    if cms_found == None:
+        return urls + " : No CMS"
 
 
-wappalyzer = Wappalyzer.latest()
-webpage = WebPage.new_from_url(domain)
-print(wappalyzer.analyze_with_categories(webpage)
+print(get_cms(domain))
+
+# список обнаруженных поддоменов
+
+domain1 = "pgu.ru"
+
+
+def get_certif(hostname):
+    ctx = ssl.create_default_context()
+    with ctx.wrap_socket(socket.socket(), server_hostname=hostname) as s:
+        s.connect((hostname, 443))
+        cert = s.getpeercert()
+
+    subject = dict(x[0] for x in cert["subject"])
+    issued_to = subject["commonName"]
+    issuer = dict(x[0] for x in cert["issuer"])
+    issued_by = issuer["commonName"]
+    print(f"Сертификат SSL: {issued_by}")
+
+
+get_certif(domain1)
+
+
+def get_subdom(subdomain):
+    discovered_subdomains = []
+    file = open("subdomains-1000.txt")
+    content = file.read()
+    subdomains = content.splitlines()
+
+    # создать URL
+    for subdomain in subdomains:
+        url = f"http://{subdomain}.{domain1}"
+        try:
+            # если возникает ОШИБКА, значит, субдомен не существует
+            requests.head(url, timeout=5)
+        except requests.ConnectionError:
+            pass
+            # если поддомена не существует, просто передать, ничего не выводить
+        else:
+            discovered_subdomains.append(url)
+            print("[+] Обнаружен поддомен:", url)
+
+
+print(get_subdom(domain))
---
+++ new_env/dzzzfff.py
@@ -0,0 +1,19 @@
+import ssl, socket
+
+domain = "pgu.ru"
+
+
+def get_certif(hostname):
+    ctx = ssl.create_default_context()
+    with ctx.wrap_socket(socket.socket(), server_hostname=hostname) as s:
+        s.connect((hostname, 443))
+        cert = s.getpeercert()
+
+    subject = dict(x[0] for x in cert["subject"])
+    issued_to = subject["commonName"]
+    issuer = dict(x[0] for x in cert["issuer"])
+    issued_by = issuer["commonName"]
+    print(f"Сертификат SSL: {issued_by}")
+
+
+get_certif(domain)
